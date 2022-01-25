757d17e66aa969d4e0a6d2d9f6f22acced64d223
jc-uni <jonas.claesson@unicus.com>
2022-01-25 14:00:16 +0100

    added calculator class

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -2,15 +2,17 @@ import javax.swing.*;
 
 public class GUI {
     public GUI(){
-        frameSetup();
-
+        var frame = frameSetup();
+        JLabel label = new JLabel("test");
+        frame.add(label);
     }
 
-    public void frameSetup(){
+    public JFrame frameSetup(){
         var frame = new JFrame();
         frame.setSize(800,600);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        return frame;
     }
 
 }
